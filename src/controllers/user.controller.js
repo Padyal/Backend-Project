4b978cf08bd9c263b3ac3bedad9d10e17f25b9bd
Padyal <atharvapadyal05@gmail.com>
2024-12-30 22:53:10 +0530

    Half user controller done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,49 @@
 import {asyncHandler} from '../utils/asyncHandler.js'
+import {ApiError} from '../utils/APIerror.js'
+//user can directly access mongo db
+import {User} from '../models/user.model.js'
 
 const registerUser = asyncHandler((req,res)=>{
-    res.status(200).json({
-        message:'Atharva Padyal'
+    //step 1 : get user credentials from users
+    //step 2 : validation of all input (!empty and correct format)
+    //step 3 : check if user already exist(username and email)
+    //step 4 : check for avatar as its required
+    //step 5 : upload them to cloudinary
+    //step 6 : Check if uplaoded correctly
+    //step 7 : Make user object - create entry in db
+    //step 8 : Remove password and refresh token field
+    //step 9 : Check if user created successfully
+    //step 10 : return res
+
+    const {fullName,email,userName,password} = req.body;
+    // normal syntax
+    // if(fullName===""){
+    //     throw new ApiError(400,'Enter valid username')
+    // }
+
+    //good syntax
+    if([fullName,userName,email,password].some((field)=>{
+        if(field){
+            if(field.trim()==="") return true;
+        }return false
+    })){
+        throw new ApiError(400,"All fields are required")
+    }
+
+    const exist = User.findOne({
+        $or:[{userName},{email}]
     })
+    if(exist){
+        throw new ApiError(409,"This User already exist")
+    }
+    
+
+    const localAvatarPath=req.files?.avatar[0]?.path
+    const localCoverPath=req.files?.coverImage[0]?.path
+
+    if(localAvatarPath){
+        throw new ApiError(400,'Avatar file is required')
+    }
 })
 
 export {registerUser}
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,27 @@
 import { Router } from "express";
 import {registerUser} from '../controllers/user.controller.js'
+import {upload} from '../middlewares/multer.middleware.js'
 
 const router =  Router()
 
 //url will be like //https://localhost:8000/users/register
-router.route("/register").post(registerUser)
+// router.route("/register").post(registerUser) without middleware
+
+//addition of multer middleware for file handling
+router.route("/register").post(
+    upload.fields([
+        //avatar
+        {
+            name:'avatar',
+            maxCount:1,
+        },
+        //coverimage
+        {
+            name:'coverImage',
+            maxCount:1,
+        }
+    ]),
+    registerUser
+)
 
 export default router
