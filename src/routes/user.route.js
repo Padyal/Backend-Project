8d643cdcd5461ecc4127665e5bcc3d7f098a876a
Padyal <atharvapadyal05@gmail.com>
2025-02-02 18:43:42 +0530

    added new routes for user comtroller

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,13 +1,27 @@
 import { Router } from "express";
-import {registerUser,loginUser,logoutUser,refreshAccessToken} from '../controllers/user.controller.js'
-import {verifyJWT} from '../middlewares/auth.middleware.js'
+import {
+    registerUser, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getUser, 
+    updateUserAvatar, 
+    updateUserCoverImage, 
+    updateAccountDetailsTextBased, 
+    getUserChannelProfile, 
+    getWatchHistory
+} 
+from '../controllers/user.controller.js'
+import { verifyJWT } from '../middlewares/auth.middleware.js'
 
 
 // we use multer to handle our files in a particular routes
-import {upload} from '../middlewares/multer.middleware.js'
+import { upload } from '../middlewares/multer.middleware.js'
+import multer from "multer";
 
 //function in express that helps bring modularity
-const router =  Router()
+const router = Router()
 
 //url will be like //https://localhost:8000/users/register
 // router.route("/register").post(registerUser) without middleware
@@ -17,19 +31,27 @@ router.route("/register").post(
     upload.fields([
         {
             //name for this field will be same as it is in front end so remembering this is imp
-            name:'avatar',
-            maxCount : 1
-        },{
-            name:'coverimage',
-            maxCount:1,
+            name: 'avatar',
+            maxCount: 1
+        }, {
+            name: 'coverimage',
+            maxCount: 1,
         }
-    ])   
-    ,registerUser)
+    ])
+    , registerUser)
 
 router.route('/login').post(loginUser)
 
 //secured routes
-router.route('/logout').post(verifyJWT,logoutUser)
+router.route('/logout').post(verifyJWT, logoutUser)
 router.route('/refresh-token').post(refreshAccessToken)
+router.route('/change-password').post(verifyJWT, changeCurrentPassword)
+router.route('/current-user').get(verifyJWT, getUser)
+router.route('/update-account').patch(verifyJWT, updateAccountDetailsTextBased)
+router.route('/avatar').patch(verifyJWT, upload.single('avatar'), updateUserAvatar)
+router.route('/cover-image').patch(verifyJWT, upload.single('coverimage'), updateUserCoverImage)
+router.route('/c/:username').get(verifyJWT, getUserChannelProfile)
+router.route('/user-history').get(verifyJWT, getWatchHistory)
+
 
 export default router
